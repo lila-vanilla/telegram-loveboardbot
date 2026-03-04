1c238315bdb6a70ee87967859ce52c18fe76f703
lila-vanilla <andrei.i.nedosekin@gmail.com>
2026-03-04 14:55:24 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ import asyncio
 from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.dispatcher.fsm.storage.memory import MemoryStorage
-from aiogram.dispatcher.fsm.context import FSMContext
-from aiogram.dispatcher.fsm.state import StatesGroup, State
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.filters import Command
 
 # --------- Настройки ---------
