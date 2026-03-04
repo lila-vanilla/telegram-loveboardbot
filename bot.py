f1b39169252178f21b0816ea0fa09a77c8704672
lila-vanilla <andrei.i.nedosekin@gmail.com>
2026-03-04 17:46:41 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.dispatcher.fsm.storage.memory import MemoryStorage
-from aiogram.dispatcher.fsm.context import FSMContext
-from aiogram.dispatcher.fsm.state import StatesGroup, State
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 import asyncio
 import requests
 
